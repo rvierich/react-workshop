281b3a0cdc9de263dc149d70acc960d4dfc2b696
Robin Vierich <robin@uken.com>
2015-06-23 20:28:31 -0400

    basic node static content server

---
+++ index.js
@@ -0,0 +1,34 @@
+var http = require('http'),
+  fs = require('fs'),
+  path = require('path'),
+  url = require('url');
+
+var mimeTypes = {
+  "html": "text/html",
+  "jpeg": "image/jpeg",
+  "jpg": "image/jpeg",
+  "png": "image/png",
+  "js": "text/javascript",
+  "css": "text/css"
+};
+
+http.createServer(function (req, res) {
+    var uri = url.parse(req.url).pathname;
+    var filename = path.join(process.cwd(), uri);
+
+    fs.exists(filename, function(exists) {
+      if (!exists) {
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.write('404 Not Found\n');
+        res.end();
+        return;
+      }
+
+      var mimeType = mimeTypes[path.extname(filename).split('.')[1]];
+      res.writeHead(200, {'Content-Type': mimeType});
+
+      var fileStream = fs.createReadStream(filename);
+      fileStream.pipe(res);
+    });
+
+}).listen(1337);
